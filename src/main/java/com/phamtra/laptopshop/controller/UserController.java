3602d369bd848ad0179b69b0bb9b14682e63cd90
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-06-06 21:05:46 +0700

    render html

--- src/main/java/com/phamtra/laptopshop/controller/UserController.java
+++ src/main/java/com/phamtra/laptopshop/controller/UserController.java
@@ -1,30 +1,40 @@
 package com.phamtra.laptopshop.controller;
 
 import com.phamtra.laptopshop.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello from controller";
-//     }
-//}
-
-@RestController
-public class UserController {
-
-
+ @Controller
+ public class UserController {
+     //    //DI: dependency injection
     private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
-    @GetMapping("")
-    public String getHomePage() {
-        return this.userService.handleHello();
-    }
+     @RequestMapping("/")
+     public String getHomePage() {
+        String test = this.userService.handleHello();
+         return "phamtra.html";
+     }
 }
+
+//@RestController
+//public class UserController {
+//
+//    //DI: dependency injection
+//    private UserService userService;
+//
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("")
+//    public String getHomePage() {
+//        return this.userService.handleHello();
+//    }
+//}
+
+
