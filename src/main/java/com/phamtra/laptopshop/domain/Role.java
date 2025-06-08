73664f25f534b603eef8047043eadad703f76a21
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-06-09 00:57:14 +0700

    add @Table to create database

--- src/main/java/com/phamtra/laptopshop/domain/Role.java
+++ src/main/java/com/phamtra/laptopshop/domain/Role.java
@@ -1,11 +1,9 @@
 package com.phamtra.laptopshop.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "roles")
 public class Role {
 
     @Id
