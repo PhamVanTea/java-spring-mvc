537cfbd06033289e60764917ecf4be547cbd6fc4
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-06-17 11:08:19 +0700

    ud detail product

--- src/main/java/com/phamtra/laptopshop/client/ItemController.java
+++ src/main/java/com/phamtra/laptopshop/client/ItemController.java
@@ -1,5 +1,7 @@
 package com.phamtra.laptopshop.client;
 
+import com.phamtra.laptopshop.domain.Product;
+import com.phamtra.laptopshop.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ItemController {
+
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
     @GetMapping("/product/{id}")
     public String getProductPage(Model model, @PathVariable long id) {
+        Product pr = this.productService.fetchProductById(id).get();
+        model.addAttribute("product", pr);
+        model.addAttribute("id", id);
         return "client/product/detail";
     }
 }
